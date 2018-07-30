5aa8bcde525b5f324db16e153c7d1536082d04f1
Paulo Castro <phlcastro@gmail.com>
2018-07-30 14:03:02 +1000

    custom build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,8 +7,6 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
@@ -28,12 +26,9 @@ export default class ClassicEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
-	Autoformat,
 	Bold,
 	Italic,
 	BlockQuote,
-	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
@@ -56,7 +51,6 @@ ClassicEditor.defaultConfig = {
 			'link',
 			'bulletedList',
 			'numberedList',
-			'imageUpload',
 			'blockQuote',
 			'undo',
 			'redo'
